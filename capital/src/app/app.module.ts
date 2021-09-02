ac583b5aaaff2b6f2b0602eb433862641771cf4a
fsa0009 <84762334+fsa0009@users.noreply.github.com>
2021-09-02 18:10:37 -0400

    nav bar and footer
    nav bar

---
+++ capital/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { TopNavBarComponent } from './header/top-nav-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopNavBarComponent
+    
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
