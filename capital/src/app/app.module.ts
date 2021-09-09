ab4ee4e4fa34b4cbb989f265d29783b8eb3ef3d6
fsa0009 <84762334+fsa0009@users.noreply.github.com>
2021-09-09 18:34:50 -0400

    lab 2
    2 components added
    log in
    and image in the middle
    
    bottom fixed
    styling fixed

--- capital/src/app/app.module.ts
+++ capital/src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TopNavBarComponent } from './header/top-nav-bar.component';
-
+import { TopNavBarComponent } from './header/top-nav-bar.component'; 
+import { BottomNavBarComponent } from './footer/bottom-nav-bar.component';
+import { loginComponent } from './login/login.component';
+import { imageComponent } from './image/image.component';
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavBarComponent
-    
+    TopNavBarComponent,
+    BottomNavBarComponent,
+    loginComponent,
+    imageComponent
+
   ],
   imports: [
     BrowserModule
---
+++ capital/src/app/image/image.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'app-image',
+    templateUrl: 'image.component.html',
+    
+})
+export class imageComponent {
+
+}
